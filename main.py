763be66f656d7767bfc9c46f1168102690d61b65
bryanz35 <bryanzhong123@gmail.com>
2025-03-21 10:19:20 -0400

    more shit
    Co-Authored-By: aye1024 <165610517+aye1024@users.noreply.github.com>

--- main.py
+++ main.py
@@ -24,17 +24,20 @@ class Player:
             print("Invalid direction.")
 
     def look(self):
-        print("You're in " + rooms[self.location]["description"])
-        print("\nItems in room: ")
-        for item in rooms[self.location]["items"]:
-            print(item)
+        print("\nYou're in " + rooms[self.location]["description"])
+        if len(rooms[self.location]["items"]) != 0:
+            print("\nItems in room: ")
+            for item in rooms[self.location]["items"]:
+                print(item)
+        else:
+            print("\nThere doesn't seem to be any items in this room.")
         print("\nDirections you can go: ")
         for direction in rooms[self.location]["exits"]:
             print(direction)
 
     def inspect(self, item):
         if item in self.inventory:
-            print(item["description"])
+            print(items[item]["description"])
         else:
             print("You don't have that item.")
 
@@ -42,6 +45,7 @@ class Player:
         if item in rooms[self.location]["items"]:
             self.inventory.append(item)
             rooms[self.location]["items"].remove(item)
+            print("You took the " + item + " and placed it in your canvas bag.")
         else:
             print("Item not found in room.")
 
@@ -49,11 +53,18 @@ class Player:
         if item in self.inventory:
             self.inventory.remove(item)
             rooms[self.location]["items"].append(item)
+            print("You dropped the " + item + ".")
         else:
             print("Item not found in inventory.")
 
+    def checkInv(self):
+        print("You have:")
+        for item in self.inventory:
+            print(item)
+        print("In your inventory.")
+
     def help(self):
-        print("You can MOVE in a DIRECTION, LOOK, INSPECT and ITEM, TAKE an ITEM, or DROP an ITEM.\n")
+        print("You can MOVE in a DIRECTION, LOOK, INSPECT an ITEM, TAKE an ITEM, CHECK your INVENTORY, get HELP, or DROP an ITEM.\n")
 
 name = input("Enter your name: ")
 character = Player(name, "kitchen")
@@ -62,18 +73,17 @@ print("\nBright morning light shines through the window, you remember that you h
 character.help()
 
 while True: 
-    character.look()
-    print("What do you do next?")
-    while True:
-        i = input().lower().split()
-        if i == []:
-            print("You can't just stand idly.")
-        else:
-            break
+    print("\nWhat do you do next?")
+    i = input().lower().split()
+    if i == []:
+        print("You can't just stand idly.")
+        turns -= 1
+        continue
     turns += 1
     match i[0]:
         case "go":
             character.move(i[1])
+            character.look()
         case "look" | "l":
             character.look()
         case "quit" | "q":
@@ -86,11 +96,28 @@ while True:
             character.drop(i[1])
         case "help" | "h":
             character.help()
+        case "check" | "inventory":
+            character.checkInv()
         case _ :
+            turns -= 1
             print("That action doesn't exist.")
     if turns == 33:
-        print("\nThe sun is shining directly overhead. You get an uneasy feeling in your chest.")
+        print("\nThe sun is shining directly overhead. You get an ominous feeling.")
     elif turns == 66:
         print("\nIt's getting dark outside. Time is running out.")
     elif turns == 99:
+        sum = 0
+        for item in character.inventory:
+            sum += item["value"]
+        if sum > 20:
+            print("\nThe unearthly beast has arrived at the town, but you are adequately prepared.")
+            print("\nFaced with a sea of red decorations and unsettling noise, Nian backs off from your town.")
+            print("\nYou've saved your friends and family from certain demise this year; how will you fair next New Year's Eve?")
+            exit()
+        else:
+            print("\nThe unearthly beast has arrived at the town, wreaking havoc upon houses, families, and children.")
+            print("\nMiraculously, you're one of the few spared by the supernatural disaster.")
+            print("\nLeft with a town in shambles, you must work hard to rebuild everything your community has worked for.")
+            print("\nNext year, will you be able to protect the things you love?.")
+            exit()
         break
